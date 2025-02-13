96887b138f9f4e532929460c0afe4da97ca62b36
pushkar <pushkargupta938@gmail.com>
2025-02-13 22:39:29 +0530

    Solution to Find Day of the Week

--- Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
+++ Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
@@ -1,30 +1,14 @@
-// #include <stdio.h>
-// int main() {
-//     int a;
-//     char* arr[7] = {"Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday","Sunday"};
-//     scanf("%d",&a);
-//     printf("%s\n",arr[a+1]);
-// }
-
 #include <stdio.h>
-
 int main() {
     int a;
-    // Corrected the array to store string literals as pointers to char
-    char* arr[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-    
-    // Take input from the user
-    scanf("%d", &a);
-    
-    // Check if input is within valid range
-    if (a >= 0 && a < 7) {
-        int b = a -1;
-        // Print the corresponding day
-        printf("%s\n", arr[b]);
-    } else {
-        // Handle invalid input
+    char* arr[7] = {"Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday","Sunday"};
+    scanf("%d",&a);
+    if(a>=1 && a<=7){
+        printf("%s\n",arr[a-1]);
+    }
+    else{
         printf("Invalid");
     }
-
-    return 0;
+    
 }
+
